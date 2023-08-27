1bf815b17a19bdbf4dc6817e9097e669e561bd4f
spausum <pau@Paus-MBP.attlocal.net>
2023-08-27 11:50:42 -0400

    Create 'Share With the Community' Functionality
    Image generated from DALL-E api is stored in Cloudinary.
    MONGODB stores references to urls of the images.
    The generated image is displayed on the front page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,9 @@ import {Loader, Card, FormField} from '../components';
 
 const RenderCards = ({data, title}) => {
   if(data?.length > 0) {
-    return data.map((post) => <Card key={post._id} {...post}/>)
+    return (
+      data.map((post) => <Card key={post._id} {...post} />)
+    );
   }
 
   return (
@@ -17,6 +19,32 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState("");
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <section className="max-w-7x1 mx-auto">
@@ -52,8 +80,8 @@ const Home = () => {
                 />
               ) : (
                 <RenderCards
-                  data={[]}
-                  title="No posts found"
+                  data={allPosts}
+                  title="No posts yet"
                 />
               )}
             </div>
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,12 +1,43 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import {v2 as cloudinary} from 'cloudinary';
-
+import { v2 as cloudinary } from 'cloudinary';
 
 import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: "dmiwi2rxa",
+  api_key: "246758343543161",
+  api_secret: "PPNNiyG9A4GJGR3owQcShTinoJw"
+});
+
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+  }
+});
 
 export default router;
